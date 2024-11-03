3e6135df445042b5855d11ee1f0d83b112deef6b
Roman Slobodchickov <77662857+R0fael@users.noreply.github.com>
2024-11-03 09:22:33 -0500

    Revert "breakingCommit#1"
    This reverts commit 5e4563a27e3bccd54b8d9ccea96774391f286b7c.

--- Assets/OXO_Engine/Scripts/Systems/Timer.cs
+++ Assets/OXO_Engine/Scripts/Systems/Timer.cs
@@ -1,32 +1,14 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI text;
-    public bool isNegative;
-    public UnityEvent onStop;
-    public float time;
+    private float time;
 
     private void Update()
     {
-        if (!isNegative)
-        {
-            time += Time.deltaTime;
-        }
-        else
-        {
-            time -= Time.deltaTime;
-            if (time < 0)
-            {
-                onStop.Invoke();
-            }
-        }
+        time += Time.deltaTime;
         text.text = ((int)time).ToString();
-    }
-
-    public void AddTime(float seconds) {
-        time += seconds;
     }
 }
