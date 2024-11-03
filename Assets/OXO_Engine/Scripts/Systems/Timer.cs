5e4563a27e3bccd54b8d9ccea96774391f286b7c
Roman Slobodchickov <77662857+R0fael@users.noreply.github.com>
2024-11-03 09:21:56 -0500

    breakingCommit#1
    Leaderboard Error.

--- Assets/OXO_Engine/Scripts/Systems/Timer.cs
+++ Assets/OXO_Engine/Scripts/Systems/Timer.cs
@@ -1,14 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI text;
-    private float time;
+    public bool isNegative;
+    public UnityEvent onStop;
+    public float time;
 
     private void Update()
     {
-        time += Time.deltaTime;
+        if (!isNegative)
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            time -= Time.deltaTime;
+            if (time < 0)
+            {
+                onStop.Invoke();
+            }
+        }
         text.text = ((int)time).ToString();
+    }
+
+    public void AddTime(float seconds) {
+        time += seconds;
     }
 }
